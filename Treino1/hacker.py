b06dc41bd99d79946d174331685b7cce38deac41
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-02-16 20:15:42 +0000

    Create hacker.py

---
+++ Treino1/hacker.py
@@ -0,0 +1,18 @@
+def hacker(log):
+    d = {}
+    l = []
+    for transaction in log:
+        if transaction[1] not in d:
+            d[transaction[1]] = ['*'] * 16
+        
+        indexes = [i for i in range(16) if transaction[0][i] != '*']
+        for i in indexes:
+            d[transaction[1]][i] = transaction[0][i]
+    
+    
+    for email in d:
+        l.append(("".join(d[email]),email))
+    
+    l.sort(key=lambda t: t[1])
+    l.sort(key=lambda t: 16 - t[0].count('*'), reverse=True)
+    return l
